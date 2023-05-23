405eb6d4a40c69513c3688abdc39000ab4f4a763
Alberto Suarez <alberto.suarez8@outlook.com>
2023-05-23 14:16:33 -0400

    Added update post page

---
+++ TechBlog/Pages/UpdatePost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechBlog.Pages
+{
+    public class UpdatePostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
