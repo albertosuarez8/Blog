c6bb9164fdc08c492e7ffba2470f283369318920
Alberto Suarez <alberto.suarez8@outlook.com>
2023-05-23 13:40:25 -0400

    Created Dashboard and CreatePost pages

---
+++ TechBlog/Pages/CreatePost.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechBlog.Pages
+{
+    public class CreatePostModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
